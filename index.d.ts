726cab75726e66ae6326283609cb68911abc4b48
Ozzy Walsh <ozzy_walsh@trendmicro.com>
2019-05-08 11:39:28 +0100

    Fix PaipOptions

--- index.d.ts
+++ index.d.ts
@@ -1,54 +1,52 @@
-declare module 'paip' {
-  interface PaipRequest {
-    service: string;
-    subject: string;
-    args: Array<any>;
-    metadata?: object;
-    tx: string;
-    time: Date;
-    isPaipRequest: boolean;
-  }
-
-  interface PaipResponse {
-    service: string;
-    subject: string;
-    statusCode: number;
-    payload?: any;
-    error?: object;
-    tx: string;
-    time: Date;
-    isPaipRequest: boolean;
-  }
-
-  interface PaipNotice {
-    service: string;
-    subject: string;
-    payload: object;
-    metadata?: any;
-    tx: string;
-    time: Date;
-    isPaipNotice: boolean;
-  }
-
-  interface PaipServer {
-    id: string;
-    ready(): Promise<any>,
-    expose(subject: string, handler: (request: PaipRequest) => any): void;
-    sendRequest(request: PaipRequest): Promise<PaipResponse>;
-    sendNotice(request: PaipRequest): Promise<void>;
-    shutdown(): Promise<void>;
-    getFullName(): string;
-  }
-
-  interface PaipOptions {
-    name: string;
-    namespace?: string;
-    nats: string | Array<string>;
-    timeout: number;
-    log: string;
-  }
-
-  function Paip (options: PaipOptions): PaipServer;
-
-  export = Paip;
+interface PaipRequest {
+  service: string;
+  subject: string;
+  args: Array<any>;
+  metadata?: object;
+  tx: string;
+  time: Date;
+  isPaipRequest: boolean;
 }
+
+interface PaipResponse {
+  service: string;
+  subject: string;
+  statusCode: number;
+  payload?: any;
+  error?: object;
+  tx: string;
+  time: Date;
+  isPaipRequest: boolean;
+}
+
+interface PaipNotice {
+  service: string;
+  subject: string;
+  payload: object;
+  metadata?: any;
+  tx: string;
+  time: Date;
+  isPaipNotice: boolean;
+}
+
+interface PaipServer {
+  id: string;
+  ready(): Promise<any>,
+  expose(subject: string, handler: (request: PaipRequest) => any): void;
+  sendRequest(request: PaipRequest): Promise<PaipResponse>;
+  sendNotice(request: PaipRequest): Promise<void>;
+  shutdown(): Promise<void>;
+  getFullName(): string;
+}
+
+interface PaipOptions {
+  name: string;
+  namespace?: string;
+  nats?: string | Array<string>;
+  timeout?: number;
+  log?: string;
+}
+
+declare function Paip (options: PaipOptions): PaipServer;
+
+export = Paip;
